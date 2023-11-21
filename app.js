67cdf285347710d8614944d5b72d3ebcec77221e
dewashington33 <dewashington@hotmail.com>
2023-11-21 16:39:55 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Checks to see if you enter a number value
+let maximum = parsetInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"))
+
+while (guess != targetNum) {
+    if (guess > targetNum) {
+        guess = parseInt(prompt("Too high! Enter a new guess:"));
+    } else {
+        guess = parseInt(prompt("Too low! Enter a new guess:"))
+    }
+}
+
+console.log;
